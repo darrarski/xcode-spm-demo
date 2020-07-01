cd7a2f879acba20919a9d53ccc62ab701c2afa71
Dariusz Rybicki <darrarski@gmail.com>
2020-07-01 23:13:14 +0200

    Add greeting to FeatureA

--- FeatureA/FeatureA.swift
+++ FeatureA/FeatureA.swift
@@ -1,13 +1,42 @@
+import Combine
 import ComposableArchitecture
+import RxCombine
+import RxSwift
 
-public struct FeatureAState {
-    public init() {}
+public struct FeatureAState: Equatable {
+    public init(
+        text: String? = nil
+    ) {
+        self.text = text
+    }
+
+    public var text: String?
 }
 
-public enum FeatureAAction {}
+public enum FeatureAAction: Equatable {
+    case greet
+    case didGreet(String)
+}
 
 public struct FeatureAEnvironment {
-    public init() {}
+    public init(greeting: @escaping () -> Observable<String>) {
+        self.greeting = greeting
+    }
+
+    public var greeting: () -> Observable<String>
 }
 
-public let featureAReducer = Reducer<FeatureAState, FeatureAAction, FeatureAEnvironment>.empty
+public let featureAReducer = Reducer<FeatureAState, FeatureAAction, FeatureAEnvironment> { state, action, env in
+    switch action {
+    case .greet:
+        return env.greeting()
+            .asPublisher()
+            .catch { _ in Empty(completeImmediately: true) }
+            .map(FeatureAAction.didGreet)
+            .eraseToEffect()
+
+    case .didGreet(let text):
+        state.text = text
+        return .none
+    }
+}
--- FeatureATests/FeatureATests.swift
+++ FeatureATests/FeatureATests.swift
@@ -1,10 +1,30 @@
 import XCTest
 @testable import FeatureA
+import ComposableArchitecture
+import RxSwift
 
 final class FeatureATests: XCTestCase {
 
-    func testExample() throws {
-        XCTAssert(true)
+    func testGreeting() throws {
+        let greetingSubject = PublishSubject<String>()
+        let store = TestStore(
+            initialState: FeatureAState(text: nil),
+            reducer: featureAReducer,
+            environment: FeatureAEnvironment(
+                greeting: { greetingSubject.asObservable() }
+            )
+        )
+
+        store.assert(
+            .send(.greet),
+            .do {
+                greetingSubject.onNext("TEST")
+                greetingSubject.onCompleted()
+            },
+            .receive(.didGreet("TEST")) {
+                $0.text = "TEST"
+            }
+        )
     }
 
 }
